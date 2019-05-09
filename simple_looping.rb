2340772b4dc5a7f8932d816b09b9fd8f762253c2
mcumm64 <mcumm64@gmail.com>
2019-05-09 00:03:04 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -37,7 +37,7 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  for say in (1..8)
+  for say in (1..number_of_times)
     puts phrase
   end
 end
