a1c18a624434da197c75cf9341144d310cd7e2a6
mcumm64 <mcumm64@gmail.com>
2019-05-09 00:06:20 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -30,7 +30,7 @@ def until_iterator(number_of_times)
   counter = 0
   phrase = "Welcome to Flatiron School's Web Development Course!"
   until counter > number_of_times
-    puts phrase
+    puts phrase #p1 c=1, p2 c=2, p3 c=3, p4 c=4, p5 c=5, p6 c=6, p7
     counter += 1
   end
 end
